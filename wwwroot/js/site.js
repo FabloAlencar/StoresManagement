e77424c19268830174421ee27ce3711f7dbb686d
Fablo Alencar <fablo.alencar@hotmail.com>
2020-05-30 14:24:33 +0200

    feat: moving fullname complete to shared

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,30 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    $("#CustomerFullName").autocomplete({
+        source: function (request, response) {
+            $.ajax({
+                url: '/Customers/Search',
+                type: 'GET',
+                cache: false,
+                data: request,
+                dataType: 'json',
+                success: function (data) {
+                    response($.map(data, function (item) {
+                        return {
+                            label: item.label,
+                            value: item.id
+                        }
+                    }))
+                }
+            });
+        },
+        minLength: 2,
+        select: function (event, ui) {
+            $('#CustomerFullName').val(ui.item.label);
+            $('#CustomerId').val(ui.item.value);
+            return false;
+        }
+    });
+});
