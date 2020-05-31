69fbb61cc91dd02a6e8e65bee7177fe5b0087d2b
FabloAlencar <57524853+FabloAlencar@users.noreply.github.com>
2020-05-31 12:35:39 +0200

    feat: applying registration date to db

---
+++ Data/Migrations/20200531102729_AddingRegistrationDateColumntoPurchaseTable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StoresManagement.Data.Migrations
+{
+    public partial class AddingRegistrationDateColumntoPurchaseTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RegistrationDate",
+                table: "Purchases",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RegistrationDate",
+                table: "Purchases");
+        }
+    }
+}
